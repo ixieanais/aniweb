d8ee5f6e5dcd21d21ba79aae17dacc4c05d00f0c
Serhii Belous <hevneebelous@gmail.com>
2026-03-05 22:08:11 +0100

    chore: update config file

--- aniweb/app/config.py
+++ aniweb/app/config.py
@@ -1,14 +1,16 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 ANIWEB_PATH = Path(os.getcwd()).parent
 PARENT_PATH = ANIWEB_PATH.parent
-DATA_PATH = PARENT_PATH / "data"
-DATABASE_PATH = DATA_PATH / "database.db"
 STATIC_PATH = ANIWEB_PATH / "static"
 TEMPLATES_PATH = ANIWEB_PATH / "templates"
 UPDATE_TIME = 1800 # 30m
 SESSION_MAX_AGE = 2592000
 SESSION_NAME = "session_id"
 
-os.makedirs(DATA_PATH, exist_ok=True)
+load_dotenv(PARENT_PATH / ".env")
+
+DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+DATABASE_URL_psycopg = DATABASE_URL.replace("asyncpg", "psycopg")
